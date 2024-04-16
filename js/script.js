3f35cf1dc7d94cf3b69498f846bc0557dfa682b0
elena_e <elenaka123@gmail.com>
2024-04-16 13:53:23 -0500

    Added more code to button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
+const button = document.querySelector("button");
+const repairList = document.querySelector(".need-repair");
+
 const createVehicle = function (type, numWheels, color) {
     const vehicle = {
         type: type,
         numWheels: numWheels,
         color: color
     }
-    return vehicle
+    return vehicle;
 };
 
 const car = createVehicle("car", 4, "blue");
@@ -17,4 +20,21 @@ for (let vehicle of myVehicles) {
     for (let key in vehicle) {
         console.log(key, vehicle[key]);
     }
-};
+}
+
+bike.needsRepair = true;
+skateboard.needsRepair = true;
+
+button.addEventListener("click", function() {
+    repairList.innerHTML = "";
+   
+    const vehicleRepairList = myVehicles.filter(function(vehicle) {
+        return vehicle.needsRepair === true;
+    });
+
+    for (let vehicle of vehicleRepairList) {
+        let li = document.createElement("li");
+        li.innerHTML = `My <span>${vehicle.type}</span> needs some 💜.`;
+        repairList.append(li);
+    }
+});
